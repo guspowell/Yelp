2e42d3d78ca0293008254829d8d3651dfab18fd9
guspowell <augustuspowell@me.com>
2015-02-20 19:52:31 +0000

    jquery working

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,13 +1,12 @@
-$(document).ready(function(), {
+$(document).ready(function() {
 
-  $('.endorsements-link').on('click', function() {
-    event.preventDefault();
+  $('.endorsements-link').on('click', function(event){
+      event.preventDefault();
 
-    var endorsementCount = $(this).siblings('endorsement-count');
+      var endorsementCount = $(this).siblings('.endorsements_count');
 
-    $.post(this.href, function(response) {
-      endorsementCount.text(response.new_endorsement_count);
-    });
-  });
-
-});
+      $.post(this.href, function(response){
+        endorsementCount.text(response.new_endorsement_count);
+    })
+  })
+})
